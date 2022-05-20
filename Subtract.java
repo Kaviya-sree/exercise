4eacb81a2b8b39b95d6f1417b6f650c2223a4a2e
rajkumar1817rj@gmail.com <rajkumar1817rj@gmail.com>
2022-05-20 11:22:01 +0530

    subtraction

---
+++ Subtract.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Subtract
+{
+    public static void main(String[] args) 
+    {
+        float n,p,l;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        n = s.nextInt();
+	  System.out.print("Enter Second Number:");
+	  p = s.nextInt();
+        l=n-p;
+        System.out.println("Subtract Two Number is: "+l);
+    }
+}
